9758d33bba6e97e3d58bd3458bd857fd556ed3e8
UdayYallatti <udayyallatti87@gmail.com>
2026-01-30 18:39:19 +0530

    App Completed

---
+++ App.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from utils import StockAPI
+
+st.set_page_config(page_title="Stock Market App", layout="wide")
+
+@st.cache_resource
+def get_stock_api():
+    return StockAPI()
+
+stock_api = get_stock_api()
+
+@st.cache_data(ttl=3600)
+def search_stock(company: str):
+    return stock_api.search_symbol(company)
+
+@st.cache_data(ttl=3600)
+def daily_data(symbol: str):
+    return stock_api.get_daily_data(symbol)
+  
+@st.cache_data(ttl=3600)
+def plot_chart(df):
+    return stock_api.plot_chart(df)
+
+
+st.title("Stock Market App")
+st.subheader("By Uday Yallatti")
+company = st.text_input("Company Name: ")
+
+if company:
+    search = search_stock(company)
+    st.dataframe(search)
+    sel_symbol = st.selectbox(label="Select Stock Symbol", options= search)
+
+    if st.button("Plot Chart"):
+        df = daily_data(sel_symbol)
+        st.dataframe(df.head())
+        chart = plot_chart(df)
+        st.plotly_chart(chart)
+        
+
+
+        
+
+
+
